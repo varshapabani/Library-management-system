4b6eedb25351d9ade4560cfc2f96db8cfe55a3f3
Varsha Pabani <118767011+varshapabani@users.noreply.github.com>
2026-08-29 20:28:21 +0500

    Add files via upload

---
+++ connect.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project;
+import java.sql.Connection;
+/**
+ *
+ * @author varsh
+ */
+public class connect {
+    public Connection Connection(){
+        Connection con = null;
+        
+        return con;
+    }
+    
+}
